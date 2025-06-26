69f090b396d117955025e3ae3cc32e1ca4323bb6
marktony14 <marktony_2001@hotmail.com>
2025-06-26 01:41:39 -0500

    agregue nuevos graficos: ingreso generado al dia y el otro reservas por tipo de habitacion

--- script.js
+++ script.js
@@ -43,4 +43,39 @@ function cargarGraficosDashboard() {
       maintainAspectRatio: false
     }
   });
+
+  const ctxIngresos = document.getElementById('graficoIngresos').getContext('2d');
+  new Chart(ctxIngresos, {
+    type: 'bar',
+    data: {
+      labels: ['10/06', '11/06', '12/06', '13/06', '14/06'],
+      datasets: [{
+        label: 'Ingresos (S/.)',
+        data: [300, 450, 380, 500, 620],
+        backgroundColor: '#9316c0'
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+
+  const ctxHabitaciones = document.getElementById('graficoHabitaciones').getContext('2d');
+  new Chart(ctxHabitaciones, {
+    type: 'bar',
+    data: {
+      labels: ['Individual', 'Doble', 'Matrimonial', 'Suite'],
+      datasets: [{
+        label: 'Reservas',
+        data: [15, 25, 10, 5],
+        backgroundColor: '#ff9900'
+      }]
+    },
+    options: {
+      indexAxis: 'y', // Barras horizontales
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
 }
